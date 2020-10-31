af912e77e5a8d61feb27c9a0c0793a7db0896bdd
jorgest7 <jorge.segovia.tormo@gmail.com>
2020-10-31 21:04:13 +0100

    First page design and minimum functionality

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
 const name = 'init file';
 console.log(`Hello ${name}`);
+
+var categorias = document.getElementsByClassName("cat-item");
+
+
+Object.keys(categorias).forEach(item => {
+    categorias[item].addEventListener("click", function() {
+        window.location = categorias[item].getAttribute("href")
+    });
+});
